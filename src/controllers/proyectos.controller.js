95f821903043818fbadcdbd812c6b87d491c8c5c
FREDYS FERNANDEZ <juniorfrey2990@gmail.com>
2022-10-20 17:52:06 -0500

    Metodo eliminar y actualizar

--- src/controllers/proyectos.controller.js
+++ src/controllers/proyectos.controller.js
@@ -12,8 +12,6 @@ export const getProyectos = async(req, res) => {
 }
 
 export const crearProyecto = async(req, res) => {
-
-
     try {
         const { nombre, prioridad, descripcion } = req.body;
         const nuevoProyecto = await Proyecto.create({
@@ -26,6 +24,34 @@ export const crearProyecto = async(req, res) => {
     } catch (error) {
         return res.status(500).json({ mensaje: error.message });
     }
+};
+
+export const actualizarProyecto = async(req, res) => {
+
+     const { id } = req.params;
+     const { nombre, prioridad, descripcion } = req.body;
+     const proyecto = await Proyecto.findByPk(id);
+     proyecto.nombre = nombre;
+     proyecto.prioridad = prioridad;
+     proyecto.descripcion = descripcion;
+
+     
+
+}
+
+export const eliminarProyecto = async(req, res) => {
+    
+    try {
+         const { id } = req.params;
+         await Proyecto.destroy({
+           where: {
+             id,
+           },
+         });
+         res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).json({ mensaje: error.message });
+    }
 
    
-};  
+}
--- src/routes/proyectos.routes.js
+++ src/routes/proyectos.routes.js
@@ -1,12 +1,17 @@
 import {Router} from 'express';
-import { crearProyecto, getProyectos } from "../controllers/proyectos.controller.js";
+import {
+  crearProyecto,
+  getProyectos,
+  actualizarProyecto,
+  eliminarProyecto,
+} from "../controllers/proyectos.controller.js";
 
 const router = Router();
 
 router.get("/proyectos", getProyectos);
 router.post("/proyectos", crearProyecto);
-router.put("/proyectos");
-router.delete("/proyectos/:id");
+router.put("/proyectos/:id", actualizarProyecto);
+router.delete("/proyectos/:id", eliminarProyecto);
 router.get("/proyectos/:id");
 
 
