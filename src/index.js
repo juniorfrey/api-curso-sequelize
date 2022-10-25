65f39e1ebdcd710af850119e43469cb281bf098d
FREDYS FERNANDEZ <juniorfrey2990@gmail.com>
2022-10-25 16:41:10 -0500

    Se crearon las variables de entorno para conexion a base de datos.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import app from "./app.js";
 import { sequelize } from "./database/database.js";
 import './models/Proyecto.js';
 import './models/Tareas.js';
-
+import { config } from "dotenv";
+config({ path: process.ENV });
 
 const main = async() => {
 
     // validar conexion
     try {
         await sequelize.sync({ force: false });
-        app.listen(5000);
-        console.log('Servidor corriendo');
+        app.listen(process.env.PORT || 3000);
+        console.log("SERVIDOR CORRIENDO EN PUERTO " + process.env.PORT);
       } catch (error) {
         console.error('No se pudo conectar a la base de datos:', error);
       }
-
-    
 }
 
 main();
