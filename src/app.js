e1e7c76d78c25ed3ca8cad5afb7314600459f51a
FREDYS FERNANDEZ <juniorfrey2990@gmail.com>
2022-11-01 15:52:30 -0500

    Se agrego libreria socket.io para trabajr websockets, ejemplo chat en tiempo real.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express  from "express";
+import * as http from 'http'; //ES 6
+import { Server } from "socket.io";
 import proyectosRutas from "./routes/proyectos.routes.js";
 import tareasRutas from "./routes/tareas.routes.js";
 import auhRutas from "./routes/auth.routes.js";
 
+
 const app = express();
+const server = http.createServer(app)
+
+const io = new Server(server);
+io.on('connection', (socket) => {
+    console.log('Usuario conectado: ' + socket.id)
+
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg);
+    })
+})
 
 app.use(express.json());
 
@@ -13,4 +26,4 @@ app.use(tareasRutas);
 app.use(auhRutas);
 
 
-export default app;
+export default server;
